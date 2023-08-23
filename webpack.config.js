b919d426571b5f7ebe9e0d97ae9f294c728c9607
salhi50 <nonosfafa@gmail.com>
2023-08-23 10:26:56 +0100

    Output basics

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,13 @@ const path = require("path");
 
 const config = {
   context: path.resolve(__dirname, "src"),
-  entry: "./main.js"
+  entry: "./main.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    clean: true,
+    assetModuleFilename: "assets/[hash][ext][query]"
+  }
 };
 
 module.exports = config;
